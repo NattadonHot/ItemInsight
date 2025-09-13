f7061b50879cb4a621a6bc2d59193ab4adba4ddb
NattadonHot <nattadon2548900@gmail.com>
2025-09-13 22:41:02 +0700

    Add login (only front)

---
+++ src/Components/Login.tsx
@@ -0,0 +1,25 @@
+import "../styles/Login.css";
+
+function Login() {
+  return (
+    <div className="login-container">
+      <h1 className="logo">
+        <span className="logo-dark">Item</span>
+        <span className="logo-orange">Insight</span>
+      </h1>
+
+      <form className="login-form">
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" placeholder="username" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" placeholder="password" />
+
+        <button type="button" className="btn-login">LOGIN</button>
+        <p className="signup-link">Signup</p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
